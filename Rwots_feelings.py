4785145a67a56236d22772005b04b10850889d83
the13nth <the13nth@gmail.com>
2016-12-21 16:29:45 +0200

    Update Rwots_feelings.py

--- Rwots_feelings.py
+++ Rwots_feelings.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 from tweepy.auth import OAuthHandler
 
-consumer_key = 'wEJI4Jc1IMQM6bgp0UV4eoCRD'
-consumer_secret = 'Cx3Ha8OQX2J0b88v3X5P3XyYaaFz74i6xAGDLFInT6rGOeDDt1'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '743342539-nNnXIylLJUIsThfNDOomiOiRqJb9XrgZ5dUXiknK'
-access_token_secret = 'AbpmtagoDIp6MFGxOgRAunFOjNRAU8cBFZSBBpgHBKqc5'
+access_token = ''
+access_token_secret = ''
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
